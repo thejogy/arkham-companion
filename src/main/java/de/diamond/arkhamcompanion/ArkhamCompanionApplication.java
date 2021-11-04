6f59e370e58968f17578791578982e9b47af5301
thejogy <meyer.joachim@gmail.com>
2021-11-04 15:05:00 +0100

    first commit

---
+++ src/main/java/de/diamond/arkhamcompanion/ArkhamCompanionApplication.java
@@ -0,0 +1,13 @@
+package de.diamond.arkhamcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkhamCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkhamCompanionApplication.class, args);
+	}
+
+}
